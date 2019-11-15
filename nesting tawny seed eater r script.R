0504bacfec3f7a8b4716cba04b9ddde160cbb174
bastazini <bastazini.vinicius@gmail.com>
2019-11-15 15:48:00 +0100

    Update nesting tawny seed eater r script.R

--- nesting tawny seed eater r script.R
+++ nesting tawny seed eater r script.R
@@ -29,18 +29,33 @@ mod.atemporal=glm(ninho~cob.solo+cob.estrat.med+alt.estrat.med+alt.estrat.alt+fo
 mod.ano=glm(ninho~cob.solo+cob.estrat.med+alt.estrat.med+alt.estrat.alt+fogo+cob.lat+decliv+ano,family="binomial", data=dados1)
 
 AICctab(mod.glmm,mod.atemporal,mod.ano,nobs=52,weights = TRUE, delta = TRUE, base = TRUE)
-mod.test<-model.sel (mod.full,mod.lm,mod.ano,rank=AIC);mod.test
+
+mod.test<-model.sel (mod.glmm,mod.atemporal,mod.ano,rank=AICc);mod.test
 av1=model.avg (get.models (mod.test, subset = delta < 2,beta ="none",rank=AIC,fit=T))
 coefTable(av1)
 varImp(mod.full, scale = FALSE)
 
-#
-#data dredging
 
-
-mod=glmulti(ninho~.,data=dados1,family="binomial",crit=aicc,method="h", level=1)
+#multimodel
+dados2=dados1[,-dados1$ano]
+mod=glmulti(ninho~.,data=dados2,family="binomial",crit=aicc,method="h", confsetsize=256,level=1)
 coeficientes=coef.glmulti(mod);coeficientes
 summary.glmulti(mod)
 weightable(mod)
 plot(mod,type="s")
+
+"Burning"= 0.2207641
+"Vegetation density"= 0.3514052
+"Height (higher stratum)"= 0.2326585
+"Bare ground (%)" = 0.9952342
+"Habitat type" = 0.2215828
+"Slope" = 0.2968510
+"Height (lower stratum)" = 0.9796466
+"Shrub cover (%)" = 0.9998258 
+ 
+teste=dados=read.table(pipe("pbpaste"), sep="\t", header=T);teste
+par(mar=c(5,7,1,1))
+barplot(teste$RI, main="", horiz=TRUE,
+        names.arg=teste$var,xlab="Relative Varibale Importance",col="black",las=1)
+
 
