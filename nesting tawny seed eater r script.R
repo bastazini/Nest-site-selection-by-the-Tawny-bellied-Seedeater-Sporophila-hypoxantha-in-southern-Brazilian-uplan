914bcf33c82464f99aa4c9288c58955cf98ce3ad
bastazini <bastazini.vinicius@gmail.com>
2020-06-26 14:01:40 +0200

    Update nesting tawny seed eater r script.R

--- nesting tawny seed eater r script.R
+++ nesting tawny seed eater r script.R
@@ -1,4 +1,3 @@
-require(caret)
 #pacotes
 require(MuMIn)
 require(lme4)
@@ -13,22 +12,18 @@ dados=read.table(pipe("pbpaste"), sep="\t", header=T);dados
 attach(dados)
 
 #Dados padronizados
-pred=(decostand(cbind(cob.solo,cob.estrat.med,alt.estrat.med,alt.estrat.alt,cob.lat,decliv),"standardize"));pred
-ano=as.factor (ano)
-fogo=as.factor (fogo)
-amb=as.factor (amb)
-dados1=cbind(ninho,ano,fogo,amb,pred);dados1
-dados1=as.data.frame(dados1)
-names(dados1)
+pred=decostand(dados,"standardize");pred
+
 #testando maodelos completos(misto com ano,sem ano, linear com ano)
 ##modelo com ano como fator aleatorio
-mod.glmm=glmer(ninho~cob.solo+cob.estrat.med+alt.estrat.med+alt.estrat.alt+fogo+cob.lat+decliv+(1|ano),family="binomial", data=dados1)
+mod.glmm=glmer(OCCUPIED...1~BARE.GROUND+HEIGHT.LOWER.STRATUM+HEIGHT.HIGHER.STRATUM+SHRUB.COVER+fogo+SLOPE+(1|ano),family="binomial", data=dados)
 #modelo sem ano
-mod.atemporal=glm(ninho~cob.solo+cob.estrat.med+alt.estrat.med+alt.estrat.alt+fogo+cob.lat+decliv,family="binomial", data=dados1)
+mod.atemporal=glm(OCCUPIED...1~HABITAT.TYPE+BARE.GROUND+SHRUB.COVER+HEIGHT.LOWER.STRATUM+HEIGHT.HIGHER.STRATUM+BURNING+VEGETATION.DENSITY+SLOPE                
+,family="binomial", data=dados)
 #modelo com ano como fator fixo
-mod.ano=glm(ninho~cob.solo+cob.estrat.med+alt.estrat.med+alt.estrat.alt+fogo+cob.lat+decliv+ano,family="binomial", data=dados1)
+mod.ano=glm(OCCUPIED...1~HABITAT.TYPE+BARE.GROUND+SHRUB.COVER+HEIGHT.LOWER.STRATUM+HEIGHT.HIGHER.STRATUM+BURNING+VEGETATION.DENSITY+SLOPE+YEAR,family="binomial", data=dados)
 
-AICctab(mod.glmm,mod.atemporal,mod.ano,nobs=52,weights = TRUE, delta = TRUE, base = TRUE)
+AICctab(mod.atemporal,mod.ano,nobs=52,weights = TRUE, delta = TRUE, base = TRUE)
 
 ###model average se fosse usar estes modelos
 mod.test<-model.sel (mod.glmm,mod.atemporal,mod.ano,rank=AICc);mod.test
@@ -38,30 +33,26 @@ varImp(mod.full, scale = FALSE)
 
 
 #multimodel inference  and variable importance
-dados2=dados1[,-dados1$ano]
-mod=glmulti(ninho~.,data=dados2,family="binomial",crit=aicc,method="h", confsetsize=256,level=1)
-coeficientes=coef.glmulti(mod);coeficientes
-summary.glmulti(mod)
+dados2=dados[,-dados$YEAR]
+dados2=cbind(OCCUPIED...1,dados2)
+mod=glmulti(OCCUPIED...1~.,data=dados,family="binomial",crit=aicc,method="h", confsetsize=256,level=1)
+coeficientes=coef.glmulti(mod,2);coeficientes
+summary.glmulti(mod,2)
 weightable(mod)
 plot(mod,type="s")
 
-####Valores de Relative variable importance ( RVI)
-"Burning"= 0.2207641
-"Vegetation density"= 0.3514052
-"Height (higher stratum)"= 0.2326585
-"Bare ground (%)" = 0.9952342
-"Habitat type" = 0.2215828
-"Slope" = 0.2968510
-"Height (lower stratum)" = 0.9796466
-"Shrub cover (%)" = 0.9998258 
+top <- weightable(mod)
+top <- top[top$aicc <= min(top$aicc) + 2,]
+
+
  
 ###Barplot  RVI
 ##importa os valores das RVI
-teste=read.table(pipe("pbpaste"), sep="\t", header=T);teste
+teste=read.table(pipe("pbpaste"), sep="\t",header=T);teste
 ##especifica as margens do graf
-par(mar=c(5,7,1,1))
+par(mar=c(5,10,1,1))
 ###barplot
 barplot(teste$RI, main="", horiz=TRUE,
-        names.arg=teste$var,xlab="Relative Varibale Importance",col="black",las=1,xlim=c(0,1))
+        names.arg=teste$X,xlab="Relative Varibale Importance",col="black",las=1,xlim=c(0,1))
 
 
